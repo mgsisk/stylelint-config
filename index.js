79ddbe97d1cc6242f1259a94b3860df412563bab
Michael Sisk <me@mgsisk.com>
2019-01-07 10:07:39 -0500

    Remove stylelint-a11y
    While good in theory, in practice this causes more trouble than it's worth.

--- index.js
+++ index.js
@@ -158,18 +158,6 @@ module.exports = {
 
     // ----- Plugins -----------------------------------------------------------
 
-    'a11y/content-property-no-static-value': true,
-    'a11y/font-size-is-readable': true,
-    'a11y/line-height-is-vertical-rhythmed': true,
-    'a11y/media-prefers-reduced-motion': true,
-    'a11y/media-prefers-color-scheme': true,
-    'a11y/no-display-none': true,
-    'a11y/no-obsolete-attribute': true,
-    'a11y/no-obsolete-element': true,
-    'a11y/no-spread-text': true,
-    'a11y/no-outline-none': true,
-    'a11y/no-text-align-justify': true,
-    'a11y/selector-pseudo-class-focus': true,
     'plugin/declaration-block-no-ignored-properties': true,
     'plugin/stylelint-no-indistinguishable-colors': true,
     'order/properties-alphabetical-order': true,
