92cf72d4906ac1e6cbea7c80b9fa75133c640d77
Michael Sisk <me@mgsisk.com>
2020-04-12 15:30:34 -0400

    Update test suites

---
+++ test/wordpress.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const tap = require('tap')
+const stylelint = require('stylelint')
+const config = require('../wordpress.js')
+
+stylelint.lint({
+  code: 'a {\n\tdisplay: inline;\n}\n',
+  config,
+}).then(result=> tap.equal(result.errored, false, 'It passes valid CSS (wordpress)'))
+
+stylelint.lint({
+  code: 'a{color:red}',
+  config,
+}).then(result=> tap.equal(result.errored, true, 'It fails invalid CSS (wordpress)'))
