6eca6329718a47b199829f4412b44f49edbf5101
Michael Sisk <me@mgsisk.com>
2020-09-22 11:48:17 -0400

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,11 +1,9 @@
-'use strict'
-
 const tap = require('tap')
 const stylelint = require('stylelint')
 const config = require('../index.js')
 
 stylelint.lint({
-  code: 'a { color: #123; }\n',
+  code: 'a {\n  color: #123;\n  font: var(--font-size-root)/0mlh var(--font-stack-root);\n}\n',
   config,
 }).then(result=> tap.equal(result.errored, false, 'It passes valid CSS (standard)'))
 
